8ab08c45c239d9b5f96c16e3048fb7a407fd90cc
EC2 Default User <ec2-user@ip-172-31-34-155.us-east-2.compute.internal>
2020-09-20 09:03:18 +0000

    first

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  # validates :title, numericality: true
+  validates :content, presence: true
+  # validates :title, length: { minimum: 5 }
+  # validates :content, length: { maximum: 100 }
 end
